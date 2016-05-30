2f78a44a19c40556504351e1f9a409426fc707c9
slawek <slawomir.klodka@wp.pl>
2016-05-30 16:50:20 +0200

    połączone kody gry, do dalszego stylowania gry

--- black-jack-open-close.js
+++ black-jack-open-close.js
@@ -10,8 +10,30 @@ $(document).ready(function() {
     });
 });
 
+$(function() {
+    $( "#start" ).click(function() {
+        var B1, B2, B3;
+        B1=Math.round(Math.random()*10);
+        B2=Math.round(Math.random()*10);
+        B3=Math.round(Math.random()*10);
 
-function endGame (B1, B2, B3) {
+        $( ".item-game-1" )
+            .animate({backgroundPositionY:0})
+            .animate({'background-position-y':3000+B1* 100},3000);
+        $( ".item-game-2" )
+            .animate({backgroundPositionY:0})
+            .animate({'background-position-y':3000+B2* 100},3000);
+        $( ".item-game-3" )
+            .animate({backgroundPositionY:0})
+            .animate({'background-position-y':3000+B3* 100},3000);
+        setTimeout(function () {
+            checkResult(B1, B2, B3);
+        }, 3500);
+
+    });
+});
+
+function checkResult (B1, B2, B3) {
     if (B1===B2 && B2===B3){
         $('#wynik').html("Wygrałeś nagrodę!");
     }
@@ -20,3 +42,5 @@ function endGame (B1, B2, B3) {
     }
 
 }
+
+
