d1d8d1118800fb042525d01dd94805abf822863d
Msrcin <kurus.mk@gmail.com>
2016-06-03 12:08:00 +0200

    zdjecia w galeri sa responsywne do max rozmiaru, plynne przewijanie po kliknieciu w menu.

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+'use strict'
+
+$(function() {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.navi').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1500); // The number here represents the speed of the scroll in milliseconds
+                return false;
+            }
+        }
+    });
+});
